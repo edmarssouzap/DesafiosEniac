c18ba9a3bfe4bf329e6e1cc21ceb6f513b92743f
Edmar Prado <edmarssouzap@gmail.com>
2022-08-08 18:57:57 -0300

    Desafio 2

---
+++ Semestre 4 - ADS/Desafios - Agosto/Desafio 2/Construtor/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Construtor
+{
+    class Aluno
+    {
+        public string Nome { get; private set; }
+        private string Sobrenome { get; set; }
+        private DateTime Nascimento { get; set; }
+        public int Idade { get; private set; }
+
+        public Aluno(string nome, string sobrenome, DateTime nascimento)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Nascimento = nascimento;
+
+            var hoje = DateTime.Today;
+            Idade = hoje.Year - nascimento.Year;
+        }
+    }
+}
---
+++ Semestre 4 - ADS/Desafios - Agosto/Desafio 2/Construtor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno("Pedro", "Silva", new DateTime(1991, 10, 10));
+            Console.WriteLine("Desafio\n\nA idade de {0} é de {1} anos.", aluno.Nome, aluno.Idade);
+            Console.ReadKey();
+        }
+    }
+}
